b904b1b03b5b3976eafd80ba96d648e2319def0b
Liancheng Fang <fangliancheng2@gmail.com>
2019-03-14 22:45:21 -0500

    Add files via upload

--- TTkernel_g.py
+++ TTkernel_g.py
@@ -6,22 +6,19 @@ import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 from torch.autograd import Variable
 import torch.nn as nn
-from torch.nn.parameter import parameter
-from .. import functional as F
-from .. import init
-from .module import Module
-from ..._jit_internal import weak_module, weak_script_method
+from vLinear import Grnn
+from vF import comp
 
 torch.backends.cudnn.deterministic = True
 torch.manual_seed(9)
 
 dtype = torch.float
-device = torch.device("cpu")
-device = torch.device("cuda:0") # Uncomment this to run on GPU
+# device = torch.device("cpu")
+device = torch.device("cuda:0")  # Uncomment this to run on GPU
 
 # Load data
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
+bsize = 250
 trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=bsize, shuffle=True, num_workers=2)
 testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
@@ -29,106 +26,25 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=bsize, shuffle=Fals
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-# batch size
-bsize = 250
+# neurons_A: emb_dim=32  num_hidden:R=64     output_size: 10       input_size: raw x length=49
+class TTRNN(nn.Module):
+# output_size only relative to the last FC layer, in mid GRNN layer, no output_size need to be mannuly specified
+    def __int__(self, input_size, num_hidden, num_neurons_A, output_size, gen):
+        super(TTRNN, self).__init__()
+        #rnn_i is an object of class Grnn
 
-num_timesteps = 16
-num_hidden = 64
-num_neurons_A = 49
-embed_dim = 32
-num_label = 10
-patch_size = 7 #4 deng fen
-generalization = "relu"
+        self.input_size = None
+        self.output_size = None
+        self.num_neurons_A = 49
+        self.num_hidden = 64
+        self.gen = 'relu'
 
-epoch_num = 300
-im_size = 28
-piece_length = int(im_size ** 2 / T)
-
-#mimic pytorch F.linear
-@torch._jit_internal.weak_script
-def comp(input, state):
-    Ax = torch.einsum('ij,bi->bj', matrix_A, input)
-
-    if gen == "prod":
-        Axh = torch.mul(Ax, state)
-    if gen == "relu":
-        Axh = torch.maximum(Ax, state)
-        Axh = tf.maximum(Axh, 0)
-
-    output = torch.einsum('ijk,bij->bk', tensor_G, Axh)
-    return output
-
-
-#mimic nn.linear
-@weak_module
-class grnn(Module):
-    __constant__ = ['bias']
-
-    def __int__(self, in_features, num_neurons_A, num_hidden, gen):
-        super(grnn, self).__init__()
-        self.num_neurons_A = num_neurons_A
-        self.num_hidden = num_hidden
-        self.in_features = in_features
-        #self.out_features = out_features
-        #num_hidden = R, num_neurons_A = M
-        self.tensor_G = Parameter(torch.Tensor(num_neurons_A, num_hidden, num_hidden))
-        self.matrix_A = Parameter(torch.Tensor(in_features, num_neurons_A))
-        self.reset_parameter()
-        self.gen = gen
-
-    def reset_parameter(self):
-        init.kaiming_uniform_(self.tensor_G, a=math.sqrt(5))
-        init.kaiming_uniform_(self.matrix_A, a=math.sqrt(5))
-
-    @weak_script_method
-    #G_RNN 2019 ICLR paper equation (10)
-    def forward(self, input, state):
-        return comp(input, state)
-
-    def extra_repr(self):
-        return 'in_feature={}, out_feature={}, num_neurons_A={}, num_hidden={}'.format(self.in_features, self.matrix_A, self.tensor_G, self.out_features is not None)
-
-
-class RNN(nn.Module):
-#output_size only relative to the last FC layer, in mid gRNN layer, no output_size need to be mannuly specified
-    def __int__(self, input_size, output_size, num_neurons_A, num_hidden, gen):
-        super(RNN, self).__init__()
-        #rnn_i is an object of class grnn
-        self.rnn1 = grnn(input_size, num_neurons_A, num_hidden, gen)
-        self.rnn2 = grnn(input_size, num_neurons_A, num_hidden, gen)
-        self.rnn3 = grnn(input_size, num_neurons_A, num_hidden, gen)
-        self.rnn4 = grnn(input_size, num_neurons_A, num_hidden, gen)
-        self.rnn5 = grnn(input_size, num_neurons_A, num_hidden, gen)
-        self.rnn6 = grnn(input_size, num_neurons_A, num_hidden, gen)
-        self.rnn7 = grnn(input_size, num_neurons_A, num_hidden, gen)
-        self.rnn8 = grnn(input_size, num_neurons_A, num_hidden, gen)
-        self.rnn9 = grnn(input_size, num_neurons_A, num_hidden, gen)
-        self.rnn10 = grnn(input_size, num_neurons_A, num_hidden, gen)
-        self.rnn11 = grnn(input_size, num_neurons_A, num_hidden, gen)
-        self.rnn12 = grnn(input_size, num_neurons_A, num_hidden, gen)
-        self.rnn13 = grnn(input_size, num_neurons_A, num_hidden, gen)
-        self.rnn14 = grnn(input_size, num_neurons_A, num_hidden, gen)
-        self.rnn15 = grnn(input_size, num_neurons_A, num_hidden, gen)
-        #self.rnn16 = grnn(input_size, num_neurons_A, num_hidden, gen)
+        self.rnn = Grnn(input_size, num_neurons_A, num_hidden, gen)
         self.last = nn.Linear(input_size, num_output)
 
     def forward(self, input, hidden):
-        hidden1 = self.rnn1(input, hidden)
-        hidden2 = self.rnn2(input, hidden1)
-        hidden3 = self.rnn3(input, hidden2)
-        hidden4 = self.rnn4(input, hidden3)
-        hidden5 = self.rnn5(input, hidden4)
-        hidden6 = self.rnn6(input, hidden5)
-        hidden7 = self.rnn7(input, hidden6)
-        hidden8 = self.rnn8(input, hidden7)
-        hidden9 = self.rnn9(input, hidden8)
-        hidden10 = self.rnn10(input, hidden9)
-        hidden11 = self.rnn11(input, hidden10)
-        hidden12 = self.rnn12(input, hidden11)
-        hidden13 = self.rnn13(input, hidden12)
-        hidden14 = self.rnn14(input, hidden13)
-        output = self.rnn15(input, hidden14)
-        return output
+        output, hidden = self.rnn(input, hidden)
+        return output, hidden
 
     def initHidden1(self):
         return torch.zeros(1, self.num_hidden)
@@ -136,7 +52,19 @@ class RNN(nn.Module):
     def inithidden2(self):
         return torch.ones(1, self.num_hidden)
 
-def onehott(label):
+
+# helper function
+
+def rgb(images):
+    image_np = images.numpy()
+    image_input = np.zeros(((bsize, 28, 28)))
+    for i in range(0, 28):
+        for j in range(0, 28):
+            image_input[:, i, j] = image_np[:, 0, i, j]
+    return image_input
+
+
+def one_hot(label):
     batch_size = bsize
     nb_digits = 10
     # Dummy input that HAS to be 2D for the scatter (you can use view(-1,1) if needed)
@@ -151,18 +79,29 @@ def onehott(label):
     return y_onehot
 
 
-# some preprocessing function
-# input shape [bsize,3,32,32], 3 represents RGB channel, take R
-def rgb(images):
-    image_np = images.numpy()
-    image_input = np.zeros(((bsize, 32, 32)))
-    for i in range(32):
-        for j in range(32):
-            # image_input[:,i,j] =(image_np[:,0,i,j] + image_np[:,1,i,j] + image_np[:,2,i,j])/3
-            image_input[:, i, j] = image_np[:, 0, i, j]
-    return image_input
+# images: batch_size * 28 * 28 for Minst
+def feature_map(bsize, images):
+    num_split = 4
+    piece_length = 49
+    temp = np.zeros(((bsize, piece_length, 16)))
+    upper1 = np.split(np.split(images, num_split, axis=1)[0], num_split, axis=2)
+    upper2 = np.split(np.split(images, num_split, axis=1)[1], num_split, axis=2)
+    upper3 = np.split(np.split(images, num_split, axis=1)[2], num_split, axis=2)
+    upper4 = np.split(np.split(images, num_split, axis=1)[3], num_split, axis=2)
 
-def test(weights_TT):
+    for i in range(0, bsize):
+        for j in range(0, 4):
+            temp[i, :, j] = (upper1[j])[i, :, :].flatten()
+        for j in range(4, 8):
+            temp[i, :, j] = (upper2[j-4])[i, :, :].flatten()
+        for j in range(8, 12):
+            temp[i, :, j] = (upper3[j-8])[i, :, :].flatten()
+        for j in range(12, 16):
+            temp[i, :, j] = (upper4[j-12])[i, :, :].flatten()
+    return temp
+
+
+def test():
     # Test part
     correct = 0
     total = 0
@@ -172,76 +111,78 @@ def test(weights_TT):
             images.to(device)
             labels.to(device)
 
+            output, hidden = tt_rnn(input, hidden)
             outputs = g_inner(weights_CP, images)
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted.to(device) == labels.to(device)).sum().item()
     return 100.0 * (correct) / total
 
-#instantiate the class
-#input_size, output_size, num_neurons_A, num_hidden, gen
-tt_rnn = RNN(num_neurons_A, 1, num_neurons_A, num_hidden, generalization)
-learning_rate = 1e-4
-print(learning_rate)
 
-param_list = []
-param_list.append(weights_CP)
-optimizer = torch.optim.Adam(param_list, lr=learning_rate)
+def train(epoch):
+    num_timesteps = 16
+    num_output = 10
+    input_size = 49
+    num_hidden = 64
+    num_neurons_A = 32
+    embed_dim = 32
+    num_label = 10
+    patch_size = 7
+    generalization = "relu"
 
-loss_list = []
-wnorm_list = []
-epoch_list = []
-test_list = []
+    # instantiate the class
+    # self, input_size, num_hidden, num_neurons_A, output_size, gen
+    tt_rnn = TTRNN()
+    tt_rnn.input_size = 49
+    tt_rnn.output_size = 10
+    tt_rnn.num_hidden = 64
+    tt_rnn.num_neurons_A = 32
+    tt_rnn.gen = "relu"
 
-for epoch in range(epoch_num+1):
-    loss_epoch = 0.0
-    wnorm_epoch = 0.0
+    learning_rate = 1e-4
+    print(learning_rate, tt_rnn.gen)
+
+    #if tt_rnn.gen == "relu":
+    hidden = tt_rnn.initHidden1()
+    #if tt_rnn.gen == "prod":
+    #    hidden = tt_rnn.inithidden2()
+    #else:
+    #    print(tt_rnn.gen)
+    #    print("error activation input")
+
+    tt_rnn.zero_grad()
+
+    bsize = 250
     for i, data in enumerate(trainloader, 0):
-
         inputs, labels = data
-        inputs.to(device)
+        print(inputs.size())
+        #inputs.to(device)
 
         if inputs.size()[0] != bsize:
             continue
-        y = labels.float().to(device)
-        y = onehott(y)
-
-        if generalization == "relu":
-            hidden = tt_rnn.initHidden1()
-        else:
-            hidden = tt_rnn.inithidden2()
-
-        #omit tt_rnn.forward() args:input hidden
-        y_predict = tt_rnn(inputs, hidden)
-        #print('y_predict', y_predict[1,:], y_predict.size())
-
-        m = torch.nn.Softmax()
-        y_predict = m(y_predict)
-
-        #print('y_predict_soft', y_predict[1,:], y_predict.size())
-        #print('y', y[1,:], y.size())
-
-
+        inputs = torch.from_numpy(rgb(inputs))
+        input = feature_map(bsize, inputs)
+        hidden = hidden
+        y = labels.float()
+        y = one_hot(y)
+        #particular: hidden = output in each time step
+        for i in range(0, num_timesteps):
+            output, hidden = tt_rnn(input[:, :, i], hidden)
+        output = tt_rnn.last(num_hidden, num_output)
+        y_predict = torch.maximum(output, dim=1)
         loss = (y_predict - y).pow(2).sum()
-        loss_epoch += loss.item()
-        print(epoch, i, "loss:", loss)
-        print(' ')
-
-        optimizer.zero_grad()
         loss.backward()
-        optimizer.step()
-        print("grad", torch.norm(weights_CP.grad.data).item())
-        print("weights", torch.norm(weights_CP.data).item())
-        wnorm_epoch += torch.norm(weights_CP.data).item()
-        # with torch.no_grad():
-        #     weights_CP -= learning_rate * weights_CP.grad
-        #     weights_CP.grad.zero_()
-    test_list.append(test(weights_CP))
-    loss_list.append(loss_epoch)
-    wnorm_list.append(wnorm_epoch)
-    epoch_list.append(epoch)
-    df = pd.DataFrame(data={'epoch': epoch_list, 'loss': loss_list, 'wnorm': wnorm_list, 'test':test_list})
-    df.to_csv('mnist_cpkernel.csv')
+
+        for p in tt_rnn.parameters():
+            p.data.add(-learning_rate, p.grad.data)
+
+        return output, loss.item
 
 
-print('Finished Training')
+epoch_num = 300
+for epoch in range(1, epoch_num):
+    train(epoch)
+print("training finished")
+
+
+
